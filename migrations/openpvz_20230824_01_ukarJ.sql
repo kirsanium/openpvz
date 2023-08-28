12d3698f7dc1f024c4653d24cf7b723bc002c341
kirsanium <kirsanium98@gmail.com>
2023-08-28 17:45:12 +0700

    fix: migration

--- migrations/openpvz_20230824_01_ukarJ.sql
+++ migrations/openpvz_20230824_01_ukarJ.sql
@@ -1,5 +1,5 @@
 -- add column notifications.source_user_id
 -- depends: openpvz_20230711_01_txT5O
 
-ALTER TABLE notifications ADD COLUMN source_user_id integer NOT NULL;
+ALTER TABLE notifications ADD COLUMN source_user_id integer;
 ALTER TABLE notifications ADD FOREIGN KEY (source_user_id) REFERENCES users (id) ON DELETE CASCADE;
