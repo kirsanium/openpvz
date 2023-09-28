fd32abdb3c6b775a75be8c8560bd65f59a0f9f3b
kirsanium <kirsanium98@gmail.com>
2023-09-29 04:31:49 +0700

    fix: caption, newlines

--- openpvz/reports.py
+++ openpvz/reports.py
@@ -17,11 +17,17 @@ _logger = getLogger(__name__)
 async def create_and_send_watches_report(office: Office, update: Update, context: BotContext):
     to = datetime.utcnow()
     since = to - timedelta(days=30)
-    with tempfile.TemporaryFile("r+", prefix=f'{office.name}{to.strftime("%Y%m%d")}', suffix=".csv") as fpw:
+    prefix = f'{office.name}{to.strftime("%Y%m%d")}'
+    with tempfile.TemporaryFile("r+", prefix=prefix, suffix=".csv") as fpw:
         await create_report(fpw, office, since, to, context.session)
         _logger.info("Report created")
         fpw.seek(0)
-        await context.bot.send_document(update.effective_chat.id, fpw, reply_markup=main_menu(context.user.role))
+        await context.bot.send_document(
+            update.effective_chat.id,
+            fpw,
+            caption=f'{prefix}.csv',
+            reply_markup=main_menu(context.user.role)
+        )
         _logger.info("Document sent")
 
 
@@ -36,11 +42,11 @@ async def create_report(file: TextIOWrapper, office: Office, since: datetime, to
         d += timedelta(days=1)
     header = ','.join(['Дата', *list(map(lambda e: e.name, employees))])
     slot_dict = {e.id: i + 1 for i, e in enumerate(employees)}
-    file.write(header)
+    file.write(f"{header}\n")
     for d in dates:
         d_notifications = list(filter(lambda n: n.created_at.date() == d, notifications))
         row = [d.strftime("%m.%d"), *["" for _ in range(len(employees))]]
         for n in d_notifications:
             slot = slot_dict[n.source_user_id]
             row[slot] = 1 if row[slot] == "" else row[slot] + 1
-        file.write(','.join(row))
+        file.write(f"{','.join(row)}\n")
